67513fa6d24313d0cf3fa3fc9f131427116adeb7
James Tunnell <jamestunnell@gmail.com>
2015-05-14 20:34:12 -0700

    Update model.R for new release of defectPrediction package.

--- src/model.R
+++ src/model.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
-'Model software defects using ARIMA time series model of historical data.
+'Model software defects using time series model of historical data. 
+Runs multiple times, once for each combination of parameters.
 
 usage:
 model.R ISSUES_FILE [options]
@@ -9,11 +10,15 @@ arguments:
 ISSUES_FILE  A text file, containing CSV-like table, with software issue data.
 
 options:
---outdir=O  Path to a directory where plots can be saved as files
---period=P  Sampling period, in days [default: 7]
---ndiffs=D  # of differences to take, for non-stationary time series data [default: 1]
---window=W  Sample window size [default: 78]
---install   Before execution, install defectPrediction package from GitHub
+--outdir=O    Path to a directory where plots can be saved as files
+--periods=P   Sampling period, in days [default: 7,14,30]
+--ndiffs=D    # of differences to take, for non-stationary time series data [default: 1,2]
+--windows=W   Sample window size [default: 24,36,48]
+--levels=L    Confidence levels (1-99), for forecast testing [default: 75,90]
+--install     Before execution, install defectPrediction package from GitHub
+--verbose     Enable verbose mode
+--startdate=S Date to start time series
+--enddate=E   Date to end time series
 ' -> doc
 
 library(docopt)
@@ -21,18 +26,20 @@ opts <- docopt(doc) # retrieve the command-line arguments
 # opts <- list(
 #   ISSUES_FILE = "~/projects/thesis/data/mongodb/issues.txt",
 #   outdir = "~/projects/thesis",
-#   srcdir = "~/projects/thesis/src",
 #   period = "30",
 #   ndiffs = "1",
 #   window = "20"
 # )
 
 issues.file = opts$ISSUES_FILE
-sampling.period = as.integer(opts$period)
-src.dir <- opts$srcdir
+periods = as.integer(unlist(strsplit(opts$period, split=",")))
+w.sizes <- as.integer(unlist(strsplit(opts$window, split=",")))
+ndiffs <- as.integer(unlist(strsplit(opts$ndiffs, split=",")))
+levels <- as.integer(unlist(strsplit(opts$levels, split=",")))
 out.dir <- opts$outdir
-ndiffs <- as.integer(opts$ndiffs)
-w.size <- as.integer(opts$window)
+verbose <- opts$verbose
+start.date <- opts$startdate
+end.date <- opts$enddate
 
 if(opts$install){
   library(devtools)
@@ -40,14 +47,22 @@ if(opts$install){
 }
 library(defectPrediction)
 
-pcts <- model.regime(
-  issues.file = opts$ISSUES_FILE,
-  sampling.period = as.integer(opts$period),
-  window.size = as.integer(opts$window),
-  ndiffs = as.integer(opts$ndiffs),
-  out.dir = opts$outdir
-)
-
-cat("\nPercent of forecasts within prediction interval:\n")
-cat(names(pcts), "\n")
-cat(paste0(sprintf("%0.4f", pcts*100), "%"), "\n")
+cat("period","w.size","ndiff", paste(levels, "conf"),"\n", sep="\t")
+for(period in periods){
+  for(ndiff in ndiffs){
+    pre.results <- pre.modeling(issues.file = opts$ISSUES_FILE,
+      sampling.period = period, ndiff = ndiff, out.dir = opts$out.dir,
+      start.date = start.date, end.date = end.date)
+    ndiff <- pre.results$ndiff
+    for(w.size in w.sizes){
+      results <- model.regime(pre.results$ts, window.size = w.size, 
+        conf.levels = levels, ndiff = ndiff,
+        out.dir = opts$outdir, verbose = verbose)
+      cat(period, w.size, ndiff, results, "\n", sep="\t")
+    }
+  }
+}
+# 
+# cat("\nPercent of forecasts within prediction interval:\n")
+# cat(names(pcts), "\n")
+# cat(paste0(sprintf("%0.4f", pcts*100), "%"), "\n")
